b5d02baa798309b32b02f27a7f26b21804671c23
Garrett Halstein <garrett.halstein@emory.edu>
2019-11-12 08:32:01 -0500

    added the new deck function

--- deck.go
+++ deck.go
@@ -1,2 +1,19 @@
-package main 
+package main
 
+type deck []string
+
+func newDeck() deck {
+
+	var cards []string
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, val := range cardValues {
+			cards = append(cards, val+" of "+suite)
+		}
+	}
+
+	return cards
+}
