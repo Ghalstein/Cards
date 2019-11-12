8cd6676dfbc41aca2f03d737bb7d856a8d2319c6
Garrett Halstein <garrett.halstein@emory.edu>
2019-11-12 09:52:31 -0500

    error handling working properly for file reader

--- deck.go
+++ deck.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type deck []string
 
+// creates a new deck of cards
 func newDeck() deck {
 
 	var cards []string
@@ -15,5 +23,20 @@ func newDeck() deck {
 		}
 	}
 
-	return cards
+	return deck(cards)
+}
+
+// reads the csv from a txt file and makes a deck
+func newDeckFromFile(filename string) deck {
+
+	byteslice, err := ioutil.ReadFile(filename)
+	if err != nil {
+
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(byteslice), ",") // will be a csv file of cards
+
+	return deck(s)
 }
